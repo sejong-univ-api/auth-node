fcb788f63c81eaf4652c797090e9b7d4ac5c3f64
JRJR519 <kkwjdfo@gmail.com>
2025-03-17 03:21:51 +0900

    feat(fetcher) : fetcher 헤더 응답 추가

--- src/util/fetcher.ts
+++ src/util/fetcher.ts
@@ -1,75 +1,104 @@
 import ConnectionError from '../error/connection-error';
 
-/* ---------------------------------------------------------------------------
-   내부 유틸리티 함수
-   --------------------------------------------------------------------------- */
-
 const _createRequestBody = (data: FetcherRequestBody, isFormEncoded: boolean): string =>
-  isFormEncoded ? new URLSearchParams(data).toString() : JSON.stringify(data);
+     isFormEncoded ? new URLSearchParams(data).toString() : JSON.stringify(data);
 
 const _createHeaders = (
-  isFormEncoded: boolean,
-  customHeaders: FetcherHeaders = {}
+     isFormEncoded: boolean,
+     customHeaders: FetcherHeaders = {}
 ): HeadersInit => ({
-  'Content-Type': isFormEncoded ? 'application/x-www-form-urlencoded' : 'application/json',
-  ...customHeaders,
+     'Content-Type': isFormEncoded ? 'application/x-www-form-urlencoded' : 'application/json',
+     ...customHeaders,
 });
 
 const _parseResponse = async (response: Response): Promise<FetcherResponse> => {
-  const contentType = response.headers.get('Content-Type') || '';
-  if (contentType.includes('application/json')) {
-    return (await response.json()) as FetcherObject;
-  }
-  if (contentType.includes('text/html')) {
-    return await response.text();
-  }
-  return await response.text(); // 기본적으로 text 반환
+     return {
+          body: await _parseBody(response),
+          headers: _parseHeaders(response),
+          cookies: _parseCookies(response),
+     };
+};
+
+const _parseHeaders = (response: Response): FetcherHeaders =>
+     Object.fromEntries(response.headers.entries());
+
+const _parseCookies = (response: Response): FetcherCookies => {
+     const cookiesHeader = response.headers.get('Set-Cookie');
+     const cookies: Record<string, string> = {};
+     if (cookiesHeader) {
+          const cookieStrings = cookiesHeader.split(/,(?=\s*[A-Za-z0-9_-]+=)/);
+          cookieStrings.forEach((cookieStr) => {
+               const parts = cookieStr.split(';')[0].trim();
+               const [key, value] = parts.split('=');
+               if (key && value) {
+                    cookies[key.trim()] = value.trim();
+               }
+          });
+     }
+     return cookies;
+};
+
+const _parseBody = async (response: Response): Promise<FetcherObject | FetcherText> => {
+     const contentType = response.headers.get('Content-Type') || '';
+     if (contentType.includes('application/json')) {
+          return (await response.json()) as FetcherObject;
+     }
+     if (contentType.includes('text/html')) {
+          return await response.text();
+     }
+     return await response.text(); // 기본적으로 text 반환
 };
 
 const _postRequest = async (
-  url: string,
-  body: FetcherRequestBody,
-  isFormEncoded: boolean,
-  customHeaders: FetcherHeaders
+     url: string,
+     body: FetcherRequestBody,
+     isFormEncoded: boolean,
+     customHeaders: FetcherHeaders
 ): Promise<FetcherResponse> => {
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: _createHeaders(isFormEncoded, customHeaders),
-    body: _createRequestBody(body, isFormEncoded),
-  });
+     const response = await fetch(url, {
+          method: 'POST',
+          headers: _createHeaders(isFormEncoded, customHeaders),
+          body: _createRequestBody(body, isFormEncoded),
+     });
 
-  if (!response.ok) {
-    throw new ConnectionError(response.statusText, response.status);
-  }
+     if (!response.ok) {
+          throw new ConnectionError(response.statusText, response.status);
+     }
 
-  return await _parseResponse(response);
+     return await _parseResponse(response);
 };
 
 export type FetcherObject = Record<string, any>;
 export type FetcherText = string;
-export type FetcherResponse = FetcherObject | FetcherText;
+
 export type FetcherPostOptions = {
-  formUrlEncoded?: boolean;
-  headers?: FetcherHeaders;
+     formUrlEncoded?: boolean;
+     headers?: FetcherHeaders;
 };
 export type FetcherHeaders = Record<string, string>;
+export type FetcherCookies = Record<string, string>;
 export type FetcherRequestBody = Record<string, any>;
+export type FetcherResponse = {
+     body: FetcherObject | FetcherText;
+     headers: FetcherHeaders;
+     cookies: FetcherCookies;
+};
 
 export interface Fetcher {
-  post(
-    url: string,
-    body: FetcherRequestBody,
-    options?: FetcherPostOptions
-  ): Promise<FetcherResponse>;
+     post(
+          url: string,
+          body: FetcherRequestBody,
+          options?: FetcherPostOptions
+     ): Promise<FetcherResponse>;
 }
 
 const fetcher: Fetcher = Object.freeze({
-  post: (
-    url: string,
-    body: FetcherRequestBody,
-    options: FetcherPostOptions = {}
-  ): Promise<FetcherResponse> =>
-    _postRequest(url, body, options.formUrlEncoded ?? false, options.headers ?? {}),
+     post: (
+          url: string,
+          body: FetcherRequestBody,
+          options: FetcherPostOptions = {}
+     ): Promise<FetcherResponse> =>
+          _postRequest(url, body, options.formUrlEncoded ?? false, options.headers ?? {}),
 });
 
 export default fetcher;
